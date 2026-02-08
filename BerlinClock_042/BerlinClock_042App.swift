56b8b2e9b7a69d4e7a11d6fa434761a8154bb592
2026-DEV2-042 <dev_042@berlinclock.com>
2026-02-08 11:36:13 +0100

    Project setup with README and gitignore

---
+++ BerlinClock_042/BerlinClock_042App.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClock_042App.swift
+//  BerlinClock_042
+//
+//  Created by Josevin Johny on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClock_042App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Berlin Clock
+
+Demonstration of TDD implementation of Berlin Clock kata.
+
+## Setup
+
+TODO
+
+## Run
+
+TODO
